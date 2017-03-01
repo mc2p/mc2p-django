783d781f868dec42c1b35b18d055a779b26d0abd
Abel Puentes Luberta <apuentes@dnestagency.com>
2017-03-01 17:32:49 +0100

    Showed key and secret key on admin site

--- django_mc2p/admin.py
+++ django_mc2p/admin.py
@@ -4,6 +4,8 @@ from .models import MC2PConfig
 
 
 class MC2PConfigAdmin(admin.ModelAdmin):
+    list_display = ['key', 'secret_key']
+
     def has_add_permission(self, request):
         return not self.model.objects.count() and super(MC2PConfigAdmin, self).has_add_permission(request)
 
